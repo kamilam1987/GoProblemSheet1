5f6ad6e0fe0ed127d956c8874b663e9083b02be0
Kamila Michel <kamilam1987@gmail.com>
2017-10-05 23:21:00 +0100

    update

--- 04FactDigitSum.go
+++ 04FactDigitSum.go
@@ -1,7 +1,7 @@
 //Author : Kamila Michel(g00340498)
 //Factorial digit sum
 //Program which calculates the sum of the digits of the factorial of a number 
-//Source code adapted from:
+//Source code adapted from: https://stackoverflow.com/questions/17564335/golang-math-big-what-is-the-max-value-of-big-int
 
 //Main package
 package main
@@ -10,16 +10,21 @@ package main
 
 import (
 	"fmt"
-	"math/big"
+	//"math/big"
 )
 
- 
+func main(){    
+    fmt.Print("Enter a positive integer : ")
+    fmt.Scan(&n)   
+    fmt.Print("Factorial is: ",factorial(n))
+}
 //Declare variable
 // uint64 is the set of all unsigned 64-bit integers.
 var fact uint64 = 1 
                       
 var i int = 1
 var n int
+var sum int
  
 //    function declaration        
 func factorial(n int) uint64 {   
@@ -31,15 +36,15 @@ func factorial(n int) uint64 {
 			//types int64 and int
             fact *= uint64(i)  
         }
+
          
-    }    
+    }   
+    sum = sum +n
+    fmt.Println(sum);
 	//return factorial
     return fact  
 }
+
  
-func main(){    
-    fmt.Print("Enter a positive integer : ")
-    fmt.Scan(&n)   
-    fmt.Print("Factorial is: ",factorial(n))
-}
+
 
--- 05GuessingGame.go
+++ 05GuessingGame.go
@@ -1,4 +1,64 @@
 //Author : Kamila Michel(g00340498)
 //Title: Guessing game
 //Guessing game where the user must guess a randomly generated number. After every guess the program tells the user whether their number was too high or too low. At the end, the number of tries should be printed. It counts only as one try if they input the same number multiple times consecutively.
-//Source code adapted from: 
+//Source code adapted from: http://ispycode.com/GO/Flow-Control/Examples/High-Low-Guessing-Game
+
+//Main package
+package main
+
+//Import format, math random and time
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//Main function
+func main () {
+
+	var guess int
+	number := numRand(1, 50)
+	//Set number of guesses to 5 as a constatnt
+	const numGuesses = 5
+	//Number of guesses taken by user
+	guessesTaken := 0
+	
+
+	//For loop is used as while loop. Loop will execute untill condition is true
+	for {
+		if guessesTaken != numGuesses {
+			
+			//fmt.Println("Number random: ", number)
+			//Asking user to pick a number from range 1-50
+			fmt.Println("Please pick number from 1-50: ")
+			fmt.Scanln(&guess)
+			//Counter
+			guessesTaken++;			
+			
+			//Checking if guess number is smaller then number
+			if guess < number {
+				fmt.Println("Sorry your guess is too low !\n")
+			//Checking if guess number is higher then numer
+			} else if guess > number {
+				fmt.Println("Sorry your guess is too high\n")
+			//Checking if guess is correct
+			} else  {
+				//Prints out if number is correct and says how many tries user used
+				fmt.Printf("Congrats! You won! It took you %d number of times\n",guessesTaken)
+				break
+			}
+		} else {
+			//Prints out when all guesses where incorrect
+			fmt.Printf("The winning number was %d and you reached number of tries\n\n", number)
+			break
+		}
+		
+	}//End of for loop
+
+	}//End of main function
+
+//numRand function generates random number between given range	
+func numRand(min, max int)int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}//End of numRand function
