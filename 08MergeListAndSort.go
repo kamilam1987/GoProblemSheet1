96e80c5b248cffe60d883a54f8ca95f4ec78005d
Kamila Michel <kamilam1987@gmail.com>
2017-10-05 09:31:17 +0100

    Comments

--- 08MergeListAndSort.go
+++ 08MergeListAndSort.go
@@ -1,15 +1,19 @@
 //Author : Kamila Michel(g00340498)
 //Title: Merge list and sort
 //Write a function that merges two sorted lists into a new sorted list
-//Source code adapted from: 
+//Source code adapted from: https://stackoverflow.com/questions/5958169/how-to-merge-two-sorted-arrays-into-a-sorted-array, 
 
+//Main package
 package main
 
+//import format
 import 
 	"fmt"
 	
 //Main function
 func main () {
+
+	//Declare two sorted arrays
 	list1 := []int{1, 4, 6}
 	list2 := []int{2, 3, 5}
 	
@@ -21,24 +25,29 @@ func main () {
 //Merge funcion
 func merge(list1 [] int, list2 [] int) []int {
 	
+	//k - counter for merged list, i - counter for list1, j - counter for list2
 		size, i, j := len(list1)+len(list2), 0, 0
-		//Result which takes size of two arrays and make a new array
-		result := make([]int, size, size)
+		//Make a new sorted list which contains list1 and list2
+		mergedList := make([]int, size, size)
 	
+		//Checking if both counters are valid, if only list1's counter are valid or if only list2's counter are valid and resizing the mergedList
 		for k := 0; k < size; k++ {
+	
 			if i > len(list1)-1 && j <= len(list2)-1 {
-				result[k] = list2[j]
+				mergedList[k] = list2[j]
 				j++
 			} else if j > len(list2)-1 && i <= len(list1)-1 {
-				result[k] = list1[i]
+				mergedList[k] = list1[i]
 				i++
+			
 			} else if list1[i] < list2[j] {
-				result[k] = list1[i]
+				mergedList[k] = list1[i]
 				i++
 			} else {
-				result[k] = list2[j]
+				mergedList[k] = list2[j]
 				j++
 			}
 		}
-		return result
+		//Return sorted array
+		return mergedList
 	}
